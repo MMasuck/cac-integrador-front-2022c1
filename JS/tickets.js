18524d6d6874960be3f4ab80c16ece7ba881c2aa
Mariel Masuck <mmasuck@hotmail.com>
2022-05-20 13:36:20 -0300

    resume btn logic

---
+++ JS/tickets.js
@@ -0,0 +1,31 @@
+const ticket = 200;
+const studentTicket = ticket - (ticket * 0.8);
+const traineeTicket = ticket - (ticket * 0.5);
+const jrTicket = ticket - (ticket * 0.15);
+let nOfTickets;
+let category;
+let total;
+
+
+function resume() {
+    nOfTickets = document.getElementById('inputNumber').value;
+    category = document.getElementById('inputCategory').value;
+
+    if (category == 'Estudiante') {
+        total = studentTicket * nOfTickets;
+        document.getElementById('showTotal').innerHTML =
+            'Total a Pagar: $' + total;
+    } else if (category == 'Trainee') {
+        total = traineeTicket * nOfTickets;
+        document.getElementById('showTotal').innerHTML =
+            'Total a Pagar: $' + total;
+    } else if (category == 'Junior') {
+        total = jrTicket * nOfTickets;
+        document.getElementById('showTotal').innerHTML =
+            'Total a Pagar: $' + total;
+    } else if (category == 'General') {
+        total = ticket * nOfTickets;
+        document.getElementById('showTotal').innerHTML =
+            'Total a Pagar: $' + total;
+    }
+}
